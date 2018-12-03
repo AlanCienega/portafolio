4e7b871290fab644f0e233c35d46395c70514ecd
AlanCienega <alan.cienega1@gmail.com>
2018-12-03 16:58:36 -0600

    loading a items

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,16 +10,31 @@ export class InfoPaginaService {
   info:InfoPAgina={};
   cargada=false;
 
+  equipo:any[]=[];
+
   constructor(private http:HttpClient ) { 
     //console.log("servicio de pagina listo !");
-    
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo(){
     //leer archivo JSON
     this.http.get('assets/data/data-pagina.json')
     
       .subscribe((resp:InfoPAgina)=>{
         this.cargada=true;
-        this.info=resp;
-        console.log(resp);        
+        this.info=resp;       
+      });
+  }
+
+  private cargarEquipo(){
+    //leer archivo JSON de firebase
+    this.http.get('https://angular-html-6ebf2.firebaseio.com/equipo.json')
+    
+      .subscribe((resp:any[])=>{
+        this.equipo=resp;
+        // console.log(resp);        
       });
   }
 }
